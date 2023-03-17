0c6aff11394fa35d95b7b58c51dd194c09f741e9
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2023-03-17 17:41:33 +0800

    feat: add api-auth middleware

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,30 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const passportJWT = require('passport-jwt')
 const bcrypt = require('bcryptjs')
+
+const JWTStrategy = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
+
 const { User, Restaurant } = require('../models')
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+}
+
+passport.use(new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
+  User.findByPk(jwtPayload.id, {
+    include: [
+      { model: Restaurant, as: 'FavoritedRestaurants' },
+      { model: Restaurant, as: 'LikeRestaurants' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
+    ]
+  })
+    .then(user => cb(null, user))
+    .catch(err => cb(err))
+}))
 
 passport.use(new LocalStrategy(
   {
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -2,14 +2,15 @@ const express = require('express')
 const router = express.Router()
 const passport = require('../../config/passport')
 
+const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth')
 const admin = require('./modules/admin')
 const restController = require('../../controllers/apis/restaurant-controller')
 const userController = require('../../controllers/apis/user-controller')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 
-router.use('/admin', admin)
+router.use('/admin', authenticated, authenticatedAdmin, admin)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
-router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', apiErrorHandler)
 
